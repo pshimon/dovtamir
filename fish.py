0ae74c40be2d034be0a87a46e063d49415fe2df0
panfil <shimon.panfil@gmail.com>
2020-07-19 09:32:28 +0300

    fish flipped

--- fish.py
+++ fish.py
@@ -16,38 +16,50 @@ fpsClock = pygame.time.Clock()
 s = pygame.display.set_mode((W, H), 0, 32)
 pygame.display.set_caption('Fish Animation')
 
-img = pygame.image.load('fugu.png')
+#img = pygame.image.load('fugu.png')
+img = pygame.image.load('fish.png')
+img1=pygame.transform.flip(img,True,False)
+wimg=img.get_rect().right-img.get_rect().left
+himg=img.get_rect().bottom-img.get_rect().top
+print(wimg,himg)
 #initial position
-x = 10.0
-y = 10.0 
+x = 0.0
+y = 0.0 
 direction = 'right'
-
-while True: # the main game loop
+run=True
+while run: # the main game loop
     s.fill(BG)
+    for event in pygame.event.get():
+        key_pressed = pygame.key.get_pressed()
+        if event.type == pygame.QUIT or key_pressed[pygame.K_ESCAPE] :
+            run=False
 
     if direction == 'right':
         x += 5
-        if x == 280:
+        s.blit(img1, (x, y))
+        if x+wimg >= W:
             direction = 'down'
     elif direction == 'down':
         y += 5
-        if y == 220:
+        s.blit(img1, (x, y))
+        if y+himg >= H:
             direction = 'left'
     elif direction == 'left':
         x -= 5
-        if x == 10:
+        s.blit(img, (x, y))
+        if x <= 0:
             direction = 'up'
     elif direction == 'up':
         y -= 5
-        if y == 10:
+        s.blit(img, (x, y))
+        if y <= 0:
             direction = 'right'
 
-    s.blit(img, (x, y))
-
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
+    
 
     pygame.display.update()
     fpsClock.tick(FPS)
+
+pygame.quit()
+sys.exit()
+
