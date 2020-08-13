4304aaa726e3a1afe99b95494798a7b159066a8e
Shimon Panfil <shimon.panfil@gmail.com>
2020-08-13 11:50:51 +0300

    starting C

---
+++ C/hello2.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+int main()  {
+    int a;
+    float b;
+    double c;
+    printf( "Please enter an integer number: " );
+    scanf( "%d", &a );
+    printf( "You entered %d\n", a );
+    printf( "Please enter an float number: " );
+    scanf( "%f", &b );
+    printf( "You entered %f\n", b );
+    printf( "Please enter an double number: " );
+    scanf( "%lf", &c );
+    printf( "You entered %f\n", c );
+
+    return 0;
+}
