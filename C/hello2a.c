b90c0b4ce516f3738c4058aad0b089c96137a7c2
Shimon Panfil <shimon.panfil@gmail.com>
2020-09-09 18:08:50 +0300

    hello2a added

--- C/hello2a.c
+++ C/hello2a.c
@@ -4,14 +4,8 @@
 int main()  {
     int n,relative;
     float b;
-    double c,d;
+    double c;
     int ret;
-    printf( "Please enter number of strikes: " );
-    ret=scanf( "%d", &n );
-    if(ret!=1) {
-        fprintf(stderr,"scanf failed\n");
-        return 1;
-    }
     printf( "Please enter type  of strike (>0 relative, 0 absolute: " );
     ret=scanf( "%d", &relative );
     if(ret!=1) {
@@ -31,19 +25,11 @@ int main()  {
         return 3;
     }
 
-    //printf("ret=%d\n",ret);
-    printf( "You entered %lf\n", c );
     if(relative) {
-    d=pow(1.0-b,n)*c;
+        n=(int) (log(DEAD_LEVEL/c)/log(1.0-b))+1;
     } else {
-        d=c-n*b;
+        n=(int) ((c-DEAD_LEVEL)/b)+1;
     }
-    printf( "after %d strikes you have %f life power\n",n,d);
-    if(d<DEAD_LEVEL ) {
-        printf("Dead!\n");
-    } else {
-        printf("Alive!\n");
-    }
-
+    printf("you need %d strikes to kill\n",n);
     return 0;
 }
